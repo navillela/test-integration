56b4afcb0b42d050fb4d19ca7ccec2d6855d2bb1
2chanwoo <navillela.808@gmail.com>
2022-10-07 02:04:00 +0900

    그냥 stop 할 거 없이 무시. +
    소스 까보니 allowDuplicates 옵션이 그냥 무시하는 것 과 같은 코드라, onDetect에서 내가 무시하는 코드를 넣으면 되는 것..

--- lib/barcode_scanner/barcode_scanner_controller.dart
+++ lib/barcode_scanner/barcode_scanner_controller.dart
@@ -9,12 +9,10 @@ class BarcodeScannerWithController extends StatefulWidget {
   const BarcodeScannerWithController({Key? key}) : super(key: key);
 
   @override
-  _BarcodeScannerWithControllerState createState() =>
-      _BarcodeScannerWithControllerState();
+  _BarcodeScannerWithControllerState createState() => _BarcodeScannerWithControllerState();
 }
 
-class _BarcodeScannerWithControllerState
-    extends State<BarcodeScannerWithController>
+class _BarcodeScannerWithControllerState extends State<BarcodeScannerWithController>
     with SingleTickerProviderStateMixin {
   String? barcode;
 
@@ -28,7 +26,6 @@ class _BarcodeScannerWithControllerState
 
   bool isStarted = true;
 
-
   @override
   Widget build(BuildContext context) {
     print("-----------------------------      build      -------------------------------------------");
@@ -36,173 +33,66 @@ class _BarcodeScannerWithControllerState
       backgroundColor: Colors.black,
       body: Builder(
         builder: (context) {
-          return Stack(
-            children: [
-              MobileScanner(
-                controller: controller,
-                fit: BoxFit.contain,
-                allowDuplicates: true,
-                // controller: MobileScannerController(
-                //   torchEnabled: true,
-                //   facing: CameraFacing.front,
-                // ),
-                onDetect: (barcode, args) async {
-                  if(isProcessing) {
-                    print("already processed - returned");
-                    return;
-                  }
-                  isProcessing = true;
-                  await controller.stop();
-                  print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ${barcode.rawValue}");
-                  print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ${args.toString()}");
-                  if(barcode.rawValue != null) {
-                    this.barcode = barcode.rawValue;
+            return Stack(
+              children: [
+                if(isProcessing)
+                  Container(color: Colors.green),
 
-                    // if(controller.isStarting) {
-                    //   controller.isStarting = false;
-                    //   print("camera stop");
-                    //
+                MobileScanner(
+                  controller: controller,
+                  fit: BoxFit.contain,
+                  allowDuplicates: true,
+                  // controller: MobileScannerController(
+                  //   torchEnabled: true,
+                  //   facing: CameraFacing.front,
+                  // ),
+                  onDetect: (barcode, args) async {
+
+                    // if(!isProcessing) {
+                    //   isProcessing = true;
+                    //   print("already processed - returned");
+                    //   Future.delayed(Duration(seconds: 3), () {
+                    //     isProcessing = false;
+                    //   });
                     // }
 
-                    if(this.barcode == barcode.rawValue) {
-                      await flutterTts.speak("이미 처리된 티켓입니다.");
-                    }else {
-                      await flutterTts.speak("인증되었습니다");
-                    }
-                    await Future.delayed(Duration(seconds: 1));
 
-                    // if(!controller.isStarting) {
-                    //   controller.isStarting = true;
-                    //   print("camera start");
-                    //
-                    //   await controller.start();
-                    //  }
-                  }
-                    await controller.start();
-                    isProcessing = false;
-                },
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  alignment: Alignment.bottomCenter,
-                  height: 100,
-                  color: Colors.black.withOpacity(0.4),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      IconButton(
-                        color: Colors.white,
-                        icon: ValueListenableBuilder(
-                          valueListenable: controller.torchState,
-                          builder: (context, state, child) {
-                            if (state == null) {
-                              return const Icon(
-                                Icons.flash_off,
-                                color: Colors.grey,
-                              );
-                            }
-                            switch (state as TorchState) {
-                              case TorchState.off:
-                                return const Icon(
-                                  Icons.flash_off,
-                                  color: Colors.grey,
-                                );
-                              case TorchState.on:
-                                return const Icon(
-                                  Icons.flash_on,
-                                  color: Colors.yellow,
-                                );
-                            }
-                          },
-                        ),
-                        iconSize: 32.0,
-                        onPressed: () => controller.toggleTorch(),
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        icon: isStarted
-                            ? const Icon(Icons.stop)
-                            : const Icon(Icons.play_arrow),
-                        iconSize: 32.0,
-                        onPressed: () => setState(() {
-                          isStarted ? controller.stop() : controller.start();
-                          isStarted = !isStarted;
-                        }),
-                      ),
-                      Center(
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width - 200,
-                          height: 50,
-                          child: FittedBox(
-                            child: Text(
-                              barcode ?? 'Scan something!',
-                              overflow: TextOverflow.fade,
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .headline4!
-                                  .copyWith(color: Colors.white),
-                            ),
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        icon: ValueListenableBuilder(
-                          valueListenable: controller.cameraFacingState,
-                          builder: (context, state, child) {
-                            if (state == null) {
-                              return const Icon(Icons.camera_front);
-                            }
-                            switch (state as CameraFacing) {
-                              case CameraFacing.front:
-                                return const Icon(Icons.camera_front);
-                              case CameraFacing.back:
-                                return const Icon(Icons.camera_rear);
-                            }
-                          },
-                        ),
-                        iconSize: 32.0,
-                        onPressed: () => controller.switchCamera(),
-                      ),
-                      IconButton(
-                        color: Colors.white,
-                        icon: const Icon(Icons.image),
-                        iconSize: 32.0,
-                        onPressed: () async {
-                          final ImagePicker picker = ImagePicker();
-                          // Pick an image
-                          final XFile? image = await picker.pickImage(
-                            source: ImageSource.gallery,
-                          );
-                          if (image != null) {
-                            if (await controller.analyzeImage(image.path)) {
-                              if (!mounted) return;
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('Barcode found!'),
-                                  backgroundColor: Colors.green,
-                                ),
-                              );
-                            } else {
-                              if (!mounted) return;
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                const SnackBar(
-                                  content: Text('No barcode found!'),
-                                  backgroundColor: Colors.red,
-                                ),
-                              );
-                            }
-                          }
-                        },
-                      ),
-                    ],
-                  ),
+                    if (isProcessing) {
+                      print("already processed - returned");
+                      return;
+                    }
+                    isProcessing = true;
+
+                    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ${barcode.rawValue}");
+                    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ${args.toString()}");
+                    if (barcode.rawValue != null) {
+
+                      // if(controller.isStarting) {
+                      //   controller.isStarting = false;
+                      //   print("camera stop");
+                      //
+                      // }
+
+                      if (this.barcode == barcode.rawValue) {
+                        await flutterTts.speak("이미 처리된 티켓입니다.");
+                      } else {
+                        await flutterTts.speak("인증되었습니다");
+                      }
+                      this.barcode = barcode.rawValue;
+                      await Future.delayed(Duration(seconds: 2));
+                      isProcessing = false;
+                      // if(!controller.isStarting) {
+                      //   controller.isStarting = true;
+                      //   print("camera start");
+                      //
+                      //   await controller.start();
+                      //  }
+                    }
+                  },
                 ),
-              ),
-            ],
-          );
-        },
+              ],
+            );
+          }
       ),
     );
   }
